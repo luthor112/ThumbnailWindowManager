623861dfca20e5b33eacca309ae9ca0f75afac22
luthor112 <luthor112@gmail.com>
2022-04-12 14:20:30 +0200

    Docs + Thumbs project without MVVM

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Thumbs")]
+[assembly: AssemblyDescription("DWM Thumbnail Windows Selector")]
+[assembly: AssemblyProduct("Thumbs")]
+[assembly: AssemblyCopyright("Copyright © 2022 luthor112")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ThumbnailWindowManager
 Allows switching windows through thumbnails.
+
+Based on the [Thumbs project](https://github.com/MathewSachin/Thumbs) by [MathewSachin](https://github.com/MathewSachin).
